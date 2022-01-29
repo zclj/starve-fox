10acafba2b0370bdbe3211633670f529fb2c77c1
Stefan <info-s@tryne.se>
2022-01-29 16:50:16 +0100

    Create shuffleDeck function

--- haskell/starve-fox/src/StarveLib.hs
+++ haskell/starve-fox/src/StarveLib.hs
@@ -1,5 +1,9 @@
 module StarveLib (someFunc) where
 
+import           System.IO.Unsafe
+import qualified System.Random         as Random
+import qualified System.Random.Shuffle as Shuffle
+
 data CardValue = Two | Three | Four | Five | Six | Seven | Eight
                | Nine | Ten | Jack | Queen | King | Ace
                deriving (Show, Eq, Ord, Enum)
@@ -16,6 +20,10 @@ data Deck = Deck { cards :: [Card] }
 makeDeck :: Deck
 makeDeck = Deck { cards = [ Card v s | v <- [Two .. Ace],
                                        s <- [Spades .. Clubs]]}
+
+shuffleDeck :: Deck -> Deck
+shuffleDeck (Deck { cards = c}) =
+  Deck $ unsafePerformIO $ Random.getStdRandom $ Shuffle.shuffle' c
 
 -- $> Card Two Hearts
 
