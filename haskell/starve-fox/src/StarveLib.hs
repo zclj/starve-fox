636bfd79ded1f149ef1f7b04083f1caad861aa64
Stefan <info-s@tryne.se>
2022-01-29 16:35:51 +0100

    Make deck from comprehension of card values and suites

--- haskell/starve-fox/src/StarveLib.hs
+++ haskell/starve-fox/src/StarveLib.hs
@@ -1,19 +1,29 @@
 module StarveLib (someFunc) where
 
-data CardValue = One | Two | Three | Four | Five | Six | Seven | Eight
+data CardValue = Two | Three | Four | Five | Six | Seven | Eight
                | Nine | Ten | Jack | Queen | King | Ace
-               deriving (Show)
+               deriving (Show, Eq, Ord, Enum)
 
 data CardSuite = Spades | Hearts | Diamonds | Clubs
-               deriving (Show)
+               deriving (Show, Eq, Ord, Enum)
 
 data Card = Card { value :: CardValue
                  , suite :: CardSuite }
-          deriving (Show)
+          deriving (Show, Eq, Ord)
 
 data Deck = Deck { cards :: [Card] }
 
--- $> Card One Hearts
+makeDeck :: Deck
+makeDeck = Deck { cards = [ Card v s | v <- [Two .. Ace],
+                                       s <- [Spades .. Clubs]]}
+
+-- $> Card Two Hearts
+
+-- $> (Card Two Hearts) = (Card Two Hearts)
+
+-- $> (Card Two Hearts) > (Card Two Hearts)
+
+-- $> (Card Two Hearts) > (Card Two Hearts)
 
 someFunc :: String
 someFunc = "Starve Fox!"
