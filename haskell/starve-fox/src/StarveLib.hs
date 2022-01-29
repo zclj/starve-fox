3af2791170e22e32f14a835cc72b5bc1bd2f5b45
Stefan <info-s@tryne.se>
2022-01-29 15:42:57 +0100

    Create initial Card types

--- haskell/starve-fox/src/StarveLib.hs
+++ haskell/starve-fox/src/StarveLib.hs
@@ -1,4 +1,19 @@
 module StarveLib (someFunc) where
 
+data CardValue = One | Two | Three | Four | Five | Six | Seven | Eight
+               | Nine | Ten | Jack | Queen | King | Ace
+               deriving (Show)
+
+data CardSuite = Spades | Hearts | Diamonds | Clubs
+               deriving (Show)
+
+data Card = Card { value :: CardValue
+                 , suite :: CardSuite }
+          deriving (Show)
+
+data Deck = Deck { cards :: [Card] }
+
+-- $> Card One Hearts
+
 someFunc :: String
 someFunc = "Starve Fox!"
